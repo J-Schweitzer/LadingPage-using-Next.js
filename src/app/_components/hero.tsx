a3390d05aa384770249a49ed0e5f8395980121cf
João Schweitzer <joaoschweitzerrezende@gmail.com>
2026-02-13 11:38:12 -0400

    teste

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,8 +1,36 @@
 export function Hero() {
     return (
         <section className="bg-[#0f192e] text-[#059ad9] overflow-hidden">
+            
+            <div>
 
-            asdas   
+                <article>
+
+                    <div>
+
+                        <h1>
+                            Licitações com a IA Econect mais agilidade, precisão e transparência
+                        </h1>
+
+                        <p>
+                            Nossa inteligência artificial automatiza e gera documentos para 
+                            processos licitatórios, organizando fluxos e simplificando cada etapa. 
+                            Com isso, desburocratiza a gestão pública, elimina a morosidade
+                            e promove mais eficiência, inovação e transparência no setor.
+                        </p>
+
+                        <div>
+                            <a href="#">
+                                CONTATO
+                            </a>
+                        </div>
+
+                    </div>
+
+                </article>
+
+            </div>
+
         </section>
     );
 }
